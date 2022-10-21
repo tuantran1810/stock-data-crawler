6e74b871fbc95529a70035ee9ea1a0ceab96a902
tuantran <tuantran1810@github.com>
2022-10-21 09:01:03 +0700

    update code

---
+++ 2_trends.py
@@ -0,0 +1,31 @@
+import os
+import time
+from tqdm import tqdm
+from pytrends.request import TrendReq
+
+output_daily_fmt = './rawdata/{}/trend.csv'
+output_folder_fmt = './rawdata/{}'
+
+def main():
+    codes = None
+    with open('codes.txt', 'r') as fd:
+        codes = fd.readlines()
+    codes = [c[:3] for c in codes]
+
+    pytrends = TrendReq(hl='en-US', tz=360)
+
+    for c in tqdm(codes):
+        daily_file = output_daily_fmt.format(c)
+        folder = output_folder_fmt.format(c)
+        os.makedirs(folder, exist_ok=True)
+        if os.path.isfile(daily_file):
+            continue
+        kw_list = [c] # list of keywords to get data 
+        pytrends.build_payload(kw_list, cat=0, timeframe='2005-01-01 2022-09-30', geo='VN') 
+        data = pytrends.interest_over_time()
+        data = data.reset_index()
+        data.to_csv(daily_file)
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,6 @@
+# stock-data-crawler
+This is the crawler and processor for acquiring information of Vietnamese stock market.
+
+### Running order
+- 1_crawl.py: for acquiring daily pricing data and quarterly financial report.
+- 2_trends.py: for acquiring interest over time ratio from google trends.
