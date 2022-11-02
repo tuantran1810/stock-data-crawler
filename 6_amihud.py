3c30ed5d0303c318d920b4a33e9e47a4ff22e61f
tuantran <tuantran1810@github.com>
2022-11-02 07:42:53 +0700

    update code

--- 6_amihud.py
+++ 6_amihud.py
@@ -15,15 +15,22 @@ def date_string_to_quarter(date_str: str):
     q = int((m+2)/3)
     return '{}Q{}'.format(y, q)
 
+def get_next_quarter(quarter):
+    y_str, q_str = quarter.split('Q')
+    y, q = int(y_str), int(q_str)
+    if q < 4:
+        return '{}Q{}'.format(y, q+1)
+    return '{}Q1'.format(y+1)
+
 def process(code: str):
     print(code)
     input_price = input_price_fmt.format(code)
     folder = output_folder_fmt.format(code)
     output_file = output_liquidity_quarterly_fmt.format(code)
 
-    os.makedirs(folder, exist_ok=True)
-    if os.path.isfile(output_file):
-        return
+    # os.makedirs(folder, exist_ok=True)
+    # if os.path.isfile(output_file):
+    #     return
 
     price = pd.read_csv(input_price)
     price = price.drop([0, 1], axis=0)
@@ -40,9 +47,23 @@ def process(code: str):
     tmp = tmp[tmp['value'] > 0]
     tmp['amihud'] = 10e6*(tmp['change_abs'] + 10e-12)/tmp['value']
     tmp = tmp.drop(['value', 'change_abs'], axis=1)
-    amihud_quarterly = tmp.groupby('quarter').mean()
+    amihud_quarterly = tmp.groupby('quarter').mean().reset_index()
     amihud_quarterly['ln_amihud'] = np.log(amihud_quarterly['amihud'])
-    amihud_quarterly.to_csv(output_file)
+
+    quarter = amihud_quarterly['quarter'].to_list()
+    last_q = quarter[-1]
+
+    all_quarters = [quarter[0]]
+    while all_quarters[-1] != last_q:
+        q = all_quarters[-1]
+        nq = get_next_quarter(q)
+        all_quarters.append(nq)
+
+    tmp = pd.DataFrame({'quarter': all_quarters})
+    print(tmp)
+    amihud_quarterly = pd.merge(tmp, amihud_quarterly, on=['quarter'], how='left')
+    # amihud_quarterly.to_csv(output_file)
+    print(amihud_quarterly)
 
 def main():
     codes = None
@@ -54,5 +75,5 @@ def main():
         process(code)
 
 if __name__ == '__main__':
-    main()
-    # process('HPG')
+    # main()
+    process('SJF')
